4771424ba67bbccc758278deb36427139ed57e2a
Rafał Rutkowski <rrutkowski@gmail.com>
2022-12-02 12:25:54 +0100

    d02

---
+++ src/bin/d02.rs
@@ -0,0 +1,6 @@
+use aoc2022::d02;
+
+fn main() {
+    println!("{}", d02::run01());
+    println!("{}", d02::run02());
+}
---
+++ src/d02/mod.rs
@@ -0,0 +1,37 @@
+mod input;
+
+pub fn run01() -> i32 {
+    input::DATA
+        .lines()
+        .map(|line| match line {
+            "A X" => 4,
+            "A Y" => 8,
+            "A Z" => 3,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 7,
+            "C Y" => 2,
+            "C Z" => 6,
+            _ => 0,
+        })
+        .sum()
+}
+
+pub fn run02() -> i32 {
+    input::DATA
+        .lines()
+        .map(|line| match line {
+            "A X" => 3,
+            "A Y" => 4,
+            "A Z" => 8,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 2,
+            "C Y" => 6,
+            "C Z" => 7,
+            _ => 0,
+        })
+        .sum()
+}
