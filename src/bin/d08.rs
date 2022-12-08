96f29011b401b88f17fb0426476649d348d45aa9
Rafał Rutkowski <rrutkowski@gmail.com>
2022-12-08 13:32:36 +0100

    d08

---
+++ src/bin/d08.rs
@@ -0,0 +1,6 @@
+use aoc2022::d08;
+
+fn main() {
+    aoc2022::run(d08::solve01);
+    aoc2022::run(d08::solve02);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod d04;
 pub mod d05;
 pub mod d06;
 pub mod d07;
+pub mod d08;
 
 pub fn run<T, F>(f: F)
 where
