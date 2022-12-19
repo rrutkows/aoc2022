ac2767968f1b0a331543ed2bbd2915a517365e28
Rafał Rutkowski <rrutkowski@gmail.com>
2022-12-20 00:32:33 +0100

    d17p1

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,4 +1,4 @@
-use aoc2022::{d14, d15, d16};
+use aoc2022::{d14, d15, d16, d17};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -8,6 +8,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("d15p2", |b| b.iter(d15::solve02));
     c.bench_function("d16p1", |b| b.iter(|| d16::solve(1)));
     c.bench_function("d16p2", |b| b.iter(|| d16::solve(2)));
+    c.bench_function("d17p1", |b| b.iter(|| d17::solve(2022)));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ pub mod d13;
 pub mod d14;
 pub mod d15;
 pub mod d16;
+pub mod d17;
 
 pub fn run<T, F>(f: F)
 where
