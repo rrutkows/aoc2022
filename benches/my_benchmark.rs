e9dcfff98103bf8447b95583789f49b3dd199077
Rafał Rutkowski <rrutkowski@gmail.com>
2023-01-04 23:44:41 +0100

    d25: simplify

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,4 +1,4 @@
-use aoc2022::{d14, d15, d16, d17, d18, d19, d20, d21, d22, d23, d24};
+use aoc2022::{d14, d15, d16, d17, d18, d19, d20, d21, d22, d23, d24, d25};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -24,6 +24,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("d23p2", |b| b.iter(d23::solve02));
     c.bench_function("d24p1", |b| b.iter(d24::solve01));
     c.bench_function("d24p2", |b| b.iter(d24::solve02));
+    c.bench_function("d25p1", |b| b.iter(d25::solve01));
 }
 
 criterion_group!(benches, criterion_benchmark);
